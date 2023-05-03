76248c8dc4a883c67fda36733abd22ebdbbd27f7
Charles <charles99na@gmail.com>
2023-05-03 10:00:09 -0300

    adicionando cores e menu

--- Main.java
+++ Main.java
@@ -1,47 +1,68 @@
 package RescisaoTrabalhista;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+import RescisaoTrabalhista.util.Cores;
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
 
 		Scanner leia = new Scanner(System.in);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		Inicial();
+		keyPress();
+		leia.nextLine();
 
 		System.out.println("Digite o nome do funcionário: ");
 		String nome = leia.nextLine();
-
-		System.out.println("Digite o CPF do funcionário: ");
-		String cpf = leia.nextLine();
 
 		System.out.println("Digite o salário do funcionário: ");
 		double salario = leia.nextDouble();
 
 		leia.nextLine();
+		
 
-		System.out.println("O funcionário é CLT ou PJ? Digite 'CLT' ou 'PJ': ");
+		System.out.println("O funcionário é CLT ou PJ?('CLT/PJ'): ");
 		String tipoContrato = leia.nextLine();
 
 		if (tipoContrato.equalsIgnoreCase("CLT")) {
+			System.out.println("Digite o CPF do funcionário: ");
+			String cpf = leia.nextLine();
+			
+			
 			System.out.println("Digite o cargo do funcionário: ");
 			String cargo = leia.nextLine();
+			
 
 			System.out.println("Digite a data de admissão do funcionário (no formato dd/MM/yyyy): ");
 			LocalDate dataAdmissao = LocalDate.parse(leia.nextLine(), formatter);
+			
 
 			System.out.println("Digite a data de demissão do funcionário (no formato dd/MM/yyyy): ");
 			LocalDate dataDemissao = LocalDate.parse(leia.nextLine(), formatter);
+			
 
 			FuncionarioCLT funcionario = new FuncionarioCLT(nome, salario, dataAdmissao, dataDemissao, cpf);
 			double rescisao = funcionario.calcularRescisao();
+			
+			
 
-			System.out.println("Rescisão de " + funcionario.getNome() + ": R$" + rescisao);
+			MenuCLT(funcionario.getNome(), funcionario.getCpf(), cargo, rescisao);
+			
 		} else if (tipoContrato.equalsIgnoreCase("PJ")) {
 			System.out.println("Digite o nome da empresa contratante: ");
 			String nomeEmpresa = leia.nextLine();
+			
+			System.out.println("Digite o cargo do funcionário: ");
+			String cargo = leia.nextLine();
 
 			System.out.println("Digite o valor total recebido pelo funcionário: ");
 			double valorTotal = leia.nextDouble();
@@ -57,12 +78,73 @@ public class Main {
 			FuncionarioPJ funcionariopj = new FuncionarioPJ(nome, valorTotal, dataInicioContrato, dataFimContrato,
 					nomeEmpresa);
 			double rescisao = funcionariopj.calcularRescisao();
+			
+			
 
-			System.out.println("Rescisão de " + funcionariopj.getNome() + ": R$" + rescisao);
+			MenuPJ(funcionariopj.getNome(), cargo, funcionariopj.getCnpj(), rescisao);
+			
+			
 		} else {
 			System.out.println("Tipo de contrato inválido!");
 		}
+	}
 
-		leia.close();
+	
+
+	private static void keyPress() {
+		
+		try {
+			System.out.println("\n\nPara prosseguir tecle ENTER");
+			System.in.read();
+
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+		}
+		
+	}
+
+
+
+	private static void Inicial() {
+		
+		
+		System.out.printf(Cores.TEXT_BLUE_BOLD, Cores.ANSI_WHITE_BACKGROUND,
+				          "");
+		System.out.printf("\n  -------------------------------------------  ");
+		System.out.printf("\n             CALCULO TRABALHISTA               ");
+		System.out.printf("\n        BEM VINDOS A -----                     ");
+		System.out.printf("\n                                               ");
+		System.out.printf("\n       VENHA JÁ REALIZAR SEU CALCULO           ");
+		System.out.printf("\n       BASTA ENTRAR COM SEUS DADOS             ");
+		System.out.printf("\n                                               ");
+		System.out.printf("\n  -------------------------------------------  ");
+		
+		
+	}
+
+	private static void MenuPJ(String nome, String cargo, String cnpj, double rescisao) {
+
+		System.out.printf("\n  -------------------------------------------  ");
+		System.out.printf("\n             Calculo Trabalhista               ");
+		System.out.printf("\n  Dados do Colaborador:                        ");
+		System.out.printf("\n  Nome: %s                                     ", nome);
+		System.out.printf("\n  Cargo:%s                                     ", cargo);
+		System.out.printf("\n  CNPJ: %s                                     ", cnpj);
+		System.out.printf("\n  O Valor em R$ da sua Recisão é de: %.2f      ", rescisao);
+		System.out.printf("\n  -------------------------------------------  ");
+		
+	}
+
+	private static void MenuCLT(String nome, String cpf, String cargo, double rescisao) {
+		
+		System.out.printf("\n  ---------------------------------------------  ");
+		System.out.printf("\n             Calculo Trabalhista                 ");
+		System.out.printf("\n  Dados do Colaborador:                          ");
+		System.out.printf("\n  Nome: %s                                       ", nome);
+		System.out.printf("\n  CPF: %s                                   ", cpf);
+		System.out.printf("\n  Cargo: %s                             ", cargo);
+		System.out.printf("\n  O Valor em R$ da sua Recisão é de: %.2f ",rescisao);
+		System.out.printf("\n  ---------------------------------------------  ");
+		
 	}
 }
